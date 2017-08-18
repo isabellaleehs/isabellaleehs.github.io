f580a5a9d78a0c8badcffb47d888a5a8cb3785e1
isabellaleehs <isabella.leehs@hotmail.com>
2017-08-18 11:32:10 +0800

    contact php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,17 +18,15 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'isabella.leehs@hotmail.com';  
 $email_subject = "Website Contact Form: $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$from = 'From: ContactForm';
+
 if ($_POST['submit']) {
-        if (mail ($to, $email_subject, $email_body)) { 
+        if (mail ($to, $email_subject, $email_body, $from)) { 
             echo '<p>Your message has been sent!</p>';
         } else { 
             echo '<p>Something went wrong, go back and try again!</p>'; 
         }
-    }	
-
-//$to = 'isabella.leehs@hotmail.com';  
-//$email_subject = "Website Contact Form: $name";
-//$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+    }
 //$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 //$headers .= "Reply-To: $email_address";	
 //mail($to,$email_subject,$email_body,$headers);
