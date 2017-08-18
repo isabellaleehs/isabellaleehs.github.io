b7f5919ebfd6abdc3b25c10a0fa27fce2d2c9303
isabellaleehs <isabella.leehs@hotmail.com>
2017-08-18 11:16:27 +0800

    contact form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,21 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
 $to = 'isabella.leehs@hotmail.com';  
-$email_subject = "Website Contact Form:  $name";
+$email_subject = "Website Contact Form: $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+if ($_POST['submit']) {
+        if (mail ($to, $email_subject, $email_body)) { 
+            echo '<p>Your message has been sent!</p>';
+        } else { 
+            echo '<p>Something went wrong, go back and try again!</p>'; 
+        }
+    }	
+
+//$to = 'isabella.leehs@hotmail.com';  
+//$email_subject = "Website Contact Form: $name";
+//$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+//$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+//$headers .= "Reply-To: $email_address";	
+//mail($to,$email_subject,$email_body,$headers);
+//return true;			
 ?>
